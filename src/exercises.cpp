0feba098c2e58814ebeddec7fae6eab5419fd15d
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-04-13 18:20:35 -0400

    P1:E3

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void exercise_1(string s1) {
   // TODO: YOUR CODE HERE
+
 }
 
 void exercise_2(string s1) {
@@ -14,6 +15,14 @@ void exercise_2(string s1) {
 
 void exercise_3(string s1) {
   // TODO: YOUR CODE HERE
+  for(int i = 0; i <= 10; ++i){
+    if(s1[i] == 1){
+      cout << "Om-nom-nom :P" << endl;
+    } else if(s1[i] == 0){
+      cout << "No cake :(" << endl;
+      break;
+    }
+  }
 }
 
 void exercise_4(int n) {
