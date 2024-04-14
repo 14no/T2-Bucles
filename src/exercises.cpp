6e8c191c2c271d4d5da2dd1ba7757b9a29bd2655
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-04-14 19:56:15 -0400

    P6:E6

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -70,7 +70,7 @@ void exercise_5(int n, int k) {
 int exercise_6(int n) {
   // TODO: YOUR CODE HERE
   int res, sum = 0;
-  while (n != 0){
+  while (n > 0){
     res = n % 10;
     sum = res + sum;
     n = n / 10;
