c57b58d1be992bc011981e6cdf6e85dabbc234c7
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-04-14 02:55:48 -0400

    P1:E6

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -50,8 +50,13 @@ void exercise_5(int n, int k) {
 
 int exercise_6(int n) {
   // TODO: YOUR CODE HERE
-
-  return 0;
+  int residuo, suma = 0;
+  while (n != 0){
+    residuo = n % 10;
+    suma = residuo + suma;
+    n = n / 10;
+  }
+  return suma;
 }
 
 void exercise_7(int n) {
