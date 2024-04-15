76473d104357b0bcdf98ff011ce2373705b5fbe0
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-04-14 22:05:42 -0400

    P2:E16

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -180,14 +180,7 @@ void exercise_15(int decimal) {
 
 void exercise_16(int divident, int divider) {
   // TODO: YOUR CODE HERE
-  int c = 0;
-  int res;
-  while(divident >= divider){
-    divident -= divider;
-    ++c;
-    res = divident;
-  }
-  cout << c << res << endl;
+  
 }
 
 void exercise_17(int n) {
